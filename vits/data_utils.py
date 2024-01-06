83e9d89153c5e738ceb1e92324110e0a2f4a3514
Lucas Hideki Ueda <lucashueda@gmail.com>
2024-01-06 08:25:03 -0300

    print debug

--- vits/data_utils.py
+++ vits/data_utils.py
@@ -217,7 +217,7 @@ class TextAudioSpeakerCollate:
 
             spk[i] = row[5]
             
-            print(style_id.shape, row[7].shape, row[7])
+            # print(style_id.shape, row[7].shape, row[7])
             style_id[i] = row[7]
 
             mel_perturbed = row[6]
