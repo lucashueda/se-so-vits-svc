1e232585860ffa22cfd17127037e6a4557fac1f1
Lucas Hideki Ueda <lucashueda@gmail.com>
2024-01-06 10:36:37 -0300

    clamping in stft

--- vits_extend/stft.py
+++ vits_extend/stft.py
@@ -78,6 +78,7 @@ class TacotronSTFT(torch.nn.Module):
         -------
         mel_output: torch.FloatTensor of shape (B, n_mel_channels, T)
         """
+        y = torch.clamp(y, min=-1, max = 1)
         assert(torch.min(y.data) >= -1)
         assert(torch.max(y.data) <= 1)
 
